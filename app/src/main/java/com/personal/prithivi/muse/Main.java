ff7c2a689d3030e4946df93ea632ea17ee958700
Prithivi Raj <ptvrajsk@gmail.com>
2019-02-01 15:24:28 +0800

    Successfully Retrieved Songs from Device. Next Step: 1) Create Song Object to hold small space song data. 2) Abstract retrieval code to a retriever class.

--- app/src/main/java/com/personal/prithivi/muse/Main.java
+++ app/src/main/java/com/personal/prithivi/muse/Main.java
@@ -1,7 +1,17 @@
 package com.personal.prithivi.muse;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +19,72 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+
+        }
+
+        this.retrieveSongs();
     }
+
+
+    public boolean hasSongs(ArrayList<String> songs) {
+        return !(songs.size() == 0);
+    }
+
+
+    public ArrayList<String> retrieveSongs() {
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.DURATION
+        };
+        final String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
+        ArrayList<String> mp3Files = new ArrayList<>();
+
+        Cursor cursor = null;
+        try {
+            Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            cursor = getContentResolver().query(uri, projection, selection, null, sortOrder);
+            if( cursor != null){
+                cursor.moveToFirst();
+
+                while( !cursor.isAfterLast() ){
+                    String title = cursor.getString(0);
+                    String artist = cursor.getString(1);
+                    String path = cursor.getString(2);
+                    String displayName  = cursor.getString(3);
+                    String songDuration = cursor.getString(4);
+                    cursor.moveToNext();
+                    if(path != null && path.endsWith(".mp3")) {
+                        mp3Files.add(path);
+                    }
+                }
+
+            }
+
+            // print to see list of mp3 files
+            for( String file : mp3Files) {
+                Log.i("TAG", file);
+            }
+
+        } catch (Exception e) {
+            Log.e("TAG", e.toString());
+        }finally{
+            if( cursor != null){
+                cursor.close();
+            }
+        }
+
+        return mp3Files;
+    }
+
 }
